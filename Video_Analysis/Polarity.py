f3763e83a41c55328b3e0239a644d2851c1c1bf9
geek-manish <agrawal.k.manish@gmail.com>
2015-10-26 17:13:24 +0530

    Update Polarity.py
    update to calculate polarity for both *_NS.txt and *.txt files.

--- Video_Analysis/Polarity.py
+++ Video_Analysis/Polarity.py
@@ -1,29 +1,40 @@
-import os
+#!/usr/bin/python
+import utils
 import sys
 from textblob import TextBlob
 
+dictonary_original = {}
+dictonary_modified = {}
+
+
 ## Returns a dictonary of polarity values of each document in the directory
-def get_polarity(path) :
-    dict = {}
-    some_path = path      
-    os.chdir(some_path)
-    text_files = [f for f in os.listdir(some_path) if f.endswith('_NS.txt')]
-    for f1 in text_files:
+def get_polarity_of_original_files(path):    
+    reload(sys)
+    list_of_files = utils.get_files(path)
+    for f1 in list_of_files:
         f = open(f1,'r')
         file_content = f.read()
-        file_content = unicode(file_content,'utf-8')
-        document = TextBlob(file_content)
-        dict[f1] = document.sentiment.polarity
+        original_encoded_file_content = unicode(file_content,'utf-8')
+        document = TextBlob(original_encoded_file_content)
+        dictonary_original[f1] = document.sentiment.polarity
         f.close()
-    return dict
-
-if __name__ == '__main__':
-    d = get_polarity("C:/VideoTranscript")
-    for k,v in d.items():
-        print k
-        print v
         
-        #f = open(fl,'r')
-    
-    
+def get_polarity_of_modified_files(path):
+    reload(sys)
+    list_of_files = utils.get_files(path)
+    for f1 in list_of_files:
+        #print f1
+        key = f1
+        value = utils.convert_doc(path+"/"+f1)
+        dictonary_modified[key] = value
 
+    
+if __name__ == '__main__':
+    get_polarity_of_original_files("/home/manish/VideoTranscript")
+    for k,v in dictonary_original.items():
+        print k,v
+        
+    print "Now the modified one"
+    get_polarity_of_modified_files("/home/manish/VideoTranscript")
+    for k,v in dictonary_modified.items():
+        print k,v
